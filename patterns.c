41eabc86d050b8cb15b4bc10baac3a1fa163def7
Rahulrahul12513 <rahuldornala7@gmail.com>
2026-03-04 21:22:30 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int rows,cols,i,j;
+	printf("enter the number of rows=\ncols=");
+	scanf("%d",&rows,&cols);
+	for(i=0;i<=rows;i++)
+	{
+		for(j=0;j<=cols;j++)
+		{
+		
+			printf("$\n");
+		}
+		
+	}
+    return 0;	
+}
